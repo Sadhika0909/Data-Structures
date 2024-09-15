b0e5117de49885bd13c48534dbcb5c5b1ead0312
Sadhika0909 <abirami.somasundaram@gmail.com>
2024-09-15 10:33:57 +0200

    Graphs.py

--- Graphs.py
+++ Graphs.py
@@ -12,4 +12,24 @@ class Graph:
             self.graph.add(node1)
         if node2 not in self.graph:
             self.graph.add(node2)
-            
+        #add edge from node 1 to node 2:
+            self.graph(node2).append(node1)
+    #function to display the graph
+    def display(self):
+        for i in self.graph:
+            print(f"{i}:{self.graph[i]}")
+
+graph=Graph()
+graph.add("e")
+graph.add("s")
+graph.add("t")
+graph.add("a")
+graph.add("f")
+graph.add("r")
+graph.edge("e","s")
+graph.edge("s","a")
+graph.edge("s","f")
+graph.edge("r","t")
+
+
+graph.display()
